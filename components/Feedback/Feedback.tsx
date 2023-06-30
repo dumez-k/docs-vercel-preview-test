8d25cd7f35a0060e9089a9abb25bd4d4e68f2e11
dumez-k <kenneth.dumez@goteleport.com>
2023-06-30 16:46:51 -0400

    Test

--- components/Feedback/Feedback.tsx
+++ components/Feedback/Feedback.tsx
@@ -94,7 +94,8 @@ export default function PageWithJSbasedForm(props) {
                   id="comment"
                   name="comment"
                   value={comment}
-                  placeholder="Any additional comments..."
+                  //TODO: REMOVE
+                  placeholder="Any additional comments.........."
                   onChange={(e) => setComment(e.target.value)}
                   style={{
                     width: "100%",
